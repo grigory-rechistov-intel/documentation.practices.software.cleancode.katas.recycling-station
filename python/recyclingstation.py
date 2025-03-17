10dca9e6495106304bc472784aaf3779542d51ba
Grigory Rechistov <grigory.rechistov@intel.com>
2025-03-17 10:10:16 +0100

    Add sketch of Python solution

--- python/recyclingstation.py
+++ python/recyclingstation.py
@@ -3,6 +3,8 @@
 # It should be processed # as part of input collection by the same functions:
 # recycle() and better_recycle()
 
+from abc import ABC, abstractmethod
+
 from smelter import Smelter
 from crusher import Crusher
 from garbage import Garbage, ScrapMetal, Glass, GarbageProcessingException
@@ -16,3 +18,36 @@ def recycle(garbage_load: list[Garbage], smelter: Smelter, crusher: Crusher):
             garbage.cross(crusher)
         else:
             raise GarbageProcessingException("Unknown garbage type, blow up")
+
+
+# =======================================
+
+class RecycleCommand(ABC):
+    @abstractmethod
+    def __call__(self):
+        pass
+
+
+class SmeltCommand(RecycleCommand):
+    def __init__(self, piece: ScrapMetal, smelter: Smelter):
+        super().__init__()
+        self.piece = piece
+        self.smelter = smelter
+
+    def __call__(self):
+        self.piece.smelt(self.smelter)
+
+
+class CrossCommand(RecycleCommand):
+    def __init__(self, piece: Glass, crusher: Crusher):
+        super().__init__()
+        self.piece = piece
+        self.crusher = crusher
+
+    def __call__(self):
+        self.piece.cross(self.crusher)
+
+
+def better_recycle(garbage_commands: list[RecycleCommand]):
+    for cmd in garbage_commands:
+        cmd()
--- python/test_recyclingstation.py
+++ python/test_recyclingstation.py
@@ -5,6 +5,7 @@ import unittest
 from garbage import MetalType, Garbage, ScrapMetal, Glass
 
 from recyclingstation import Smelter, Crusher, recycle
+from recyclingstation import CrossCommand, SmeltCommand, better_recycle
 
 
 class GarbageTests(unittest.TestCase):
@@ -22,6 +23,18 @@ class SideEffectIntegration(unittest.TestCase):
 
         load = [m1, g2]
         recycle(load, sm, cr)
+
+    def test_better_recycle(self):
+        sm = Smelter()
+        cr = Crusher()
+        m1 = ScrapMetal(MetalType.Aluminum, weight=10)
+        g2 = Glass(volume=3)
+
+        m_cmd = SmeltCommand(m1, sm)
+        g_cmd = CrossCommand(g2, cr)
+
+        functional_load = [m_cmd, g_cmd]
+        better_recycle(functional_load)
 
 
 if __name__ == "__main__":
